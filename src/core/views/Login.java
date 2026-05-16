4eb342fe82ebfcaf9c3837761508d73a224e3775
Sebastian Molina <molinamolina@uninorte.edu.co>
2026-05-16 18:03:09 -0500

    remover carpetas separadas, mover a core

--- src/core/views/Login.java
+++ src/core/views/Login.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package views;
+package core.views;
 
 import com.formdev.flatlaf.FlatDarkLaf;
 import java.awt.Color;
