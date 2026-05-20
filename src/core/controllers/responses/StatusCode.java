8975b3ee753e9f5093cce62b3771965095778f8a
Sebastian Molina <molinamolina@uninorte.edu.co>
2026-05-20 13:59:03 -0500

    Add response controllers

---
+++ src/core/controllers/responses/StatusCode.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core.controllers.responses;
+
+/**
+ *
+ * @author Victus
+ */
+public abstract class StatusCode {
+
+    // Respuesta exitosa (200-204)
+    public static final int OK = 200;
+    public static final int CREATED = 201;
+    public static final int NO_CONTENT = 204;
+
+    // Respuesta de error por parte del cliente (400-404)
+    public static final int BAD_REQUEST = 400;
+    public static final int NOT_FOUND = 404;
+
+    // Respuesta de error por parte del servidor (500-501)
+    public static final int INTERNAL_SERVER_ERROR = 500;
+    public static final int NOT_IMPLEMENTED = 501;
+
+}
