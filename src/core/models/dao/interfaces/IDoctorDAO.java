5f8370355d20abd7293f4243ab6171cdefc69c33
Sebastian Molina <molinamolina@uninorte.edu.co>
2026-05-20 18:41:02 -0500

    populate DAO interfaces

---
+++ src/core/models/dao/interfaces/IDoctorDAO.java
@@ -0,0 +1,14 @@
+package core.models.dao.interfaces;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Victus
+ */
+public class IDoctorDAO {
+    
+}
