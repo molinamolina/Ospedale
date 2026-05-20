a2f6431778658fae2ebc935df1505776437b24c9
Sebastian Molina <molinamolina@uninorte.edu.co>
2026-05-20 18:48:23 -0500

    rename and populate more folders

--- src/core/validators/implementations/AppointmentValidator.java
+++ src/core/validators/implementations/AppointmentValidator.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package observers.interfaces;
+package core.validators.implementations;
 
 /**
  *
  * @author Victus
  */
-public class IObserver {
+public class AppointmentValidator {
     
 }
