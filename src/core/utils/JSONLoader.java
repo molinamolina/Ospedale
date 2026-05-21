8c4d47a8fc6b12739c1efaedef6054a897ae90a5
Sebastian Molina <molinamolina@uninorte.edu.co>
2026-05-20 22:57:51 -0500

    Update JSONLoader.java

--- src/core/utils/JSONLoader.java
+++ src/core/utils/JSONLoader.java
@@ -16,6 +16,7 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDate;
 
 public class JSONLoader {
 
@@ -26,12 +27,28 @@ public class JSONLoader {
             String content = new String(Files.readAllBytes(Paths.get("json/users.json")), StandardCharsets.UTF_8);
             JSONObject root = new JSONObject(content);
             JSONArray userArray = root.getJSONArray("users");
-            
-            for (int i = 0; i < userArray.length(); i++ ) {
-                // FILL THIS OUT 
+
+            for (int i = 0; i < userArray.length(); i++) {
+                JSONObject jsonfile = userArray.getJSONObject(i);
+                String type = jsonfile.getString("type");
+                long id = jsonfile.getLong("id");
+                String username = jsonfile.getString("username");
+                String firstname = jsonfile.getString("firstname");
+                String lastname = jsonfile.getString("lastname");
+                String pass = jsonfile.getString("password");
+                String email = jsonfile.getString("email");
+                LocalDate birthdate = LocalDate.parse(jsonfile.getString("birthdate"));
+                boolean gender = jsonfile.getBoolean("gender");
+                long phone = jsonfile.getLong("phone");
+                String address = jsonfile.getString("address");
+                // add specialty
+                String licenceNumber = jsonfile.getString("licencenumber");
+                String assignedOffice = jsonfile.getString("assignedOffice");
+                
+                // FILL THIS OUT!!!
             }
 
-        } catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
 
