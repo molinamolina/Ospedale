d773a7a3cf4375d77daf87e695f61bf57d6b2c07
Asaeza07 <asaeza@uninorte.edu.co>
2026-05-21 11:53:46 -0500

    Implement appointment creation and retrieval methods
    cambios

--- src/core/controllers/implementations/AppointmentController.java
+++ src/core/controllers/implementations/AppointmentController.java
@@ -3,11 +3,41 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package core.controllers.implementations;
+import core.controllers.responses.Response;
+import core.controllers.responses.StatusCode;
+import core.models.entities.Appointment;
 
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author Victus
  */
 public class AppointmentController {
-    
+     private List<Appointment> appointments;
+
+    public AppointmentController() {
+
+        appointments = new ArrayList<>();
+    }
+
+    public Response<Appointment> crearCita(Appointment appointment) {
+
+        appointments.add(appointment);
+
+        return new Response<>(
+                StatusCode.SUCCESS,
+                "Cita creada correctamente",
+                appointment
+        );
+    }
+
+    public Response<List<Appointment>> obtenerCitas() {
+
+        return new Response<>(
+                StatusCode.SUCCESS,
+                "Lista de citas obtenida",
+                appointments
+        );
+    }
 }
