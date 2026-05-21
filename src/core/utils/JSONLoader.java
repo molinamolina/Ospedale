798638bc0c61ce804420bd5387a5bbdd78a130c9
Sebastian Molina <molinamolina@uninorte.edu.co>
2026-05-20 20:21:27 -0500

    wip loader

--- src/core/utils/JSONLoader.java
+++ src/core/utils/JSONLoader.java
@@ -8,6 +8,30 @@ package core.utils;
  *
  * @author Victus
  */
+import core.repositories.DataStorage;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class JSONLoader {
-    
+
+    public static void userLoad() {
+
+        DataStorage storage = DataStorage.getInstance();
+        try {
+            String content = new String(Files.readAllBytes(Paths.get("json/users.json")), StandardCharsets.UTF_8);
+            JSONObject root = new JSONObject(content);
+            JSONArray userArray = root.getJSONArray("users");
+            
+            // FILL THIS OUT
+
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
 }
