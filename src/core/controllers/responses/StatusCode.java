ddccd76c91eedde1adeff2dc8a10b012f4f5ffbb
Sebastian Molina <molinamolina@uninorte.edu.co>
2026-05-21 22:27:05 -0500

    Add some more status codes
    418 is very much real, by the way.

--- src/core/controllers/responses/StatusCode.java
+++ src/core/controllers/responses/StatusCode.java
@@ -17,10 +17,15 @@ public abstract class StatusCode {
 
     // Respuesta de error por parte del cliente (400-404)
     public static final int BAD_REQUEST = 400;
+    public static final int UNAUTHORIZED = 401;
+    public static final int FORBIDDEN = 403;
     public static final int NOT_FOUND = 404;
+    // public static final int I_M_A_TEAPOT = 418;
 
     // Respuesta de error por parte del servidor (500-501)
     public static final int INTERNAL_SERVER_ERROR = 500;
     public static final int NOT_IMPLEMENTED = 501;
+    public static final int BAD_GATEWAY = 502;
+    public static final int SERVICE_UNAVAILABLE = 503;
 
 }
